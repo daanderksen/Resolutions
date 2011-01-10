ad3b44853ac19b7fd65fa5c08a238319788c42be
s4dror <s4dror@s4dror-VirtualBox.(none)>
2011-01-10 11:02:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resolutions::Application.config.secret_token = '9fb83be7146313a1cffc685181648fdc66754807ad217410074848adebc26a496b8b9c64a2d4975a03ee2ee9e5ea54fe79df81dd19e445e5d36f772737d65cfd'
