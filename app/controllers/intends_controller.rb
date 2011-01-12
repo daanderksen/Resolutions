3b5b71fbfadb6546721aa05458c056f2290dea50
Bjorn Post <bjorn.post@gmail.com>
2011-01-12 10:48:25 +0100

    Added delete intention button to resolution

--- app/controllers/intends_controller.rb
+++ app/controllers/intends_controller.rb
@@ -1,4 +1,7 @@
 class IntendsController < ApplicationController
+
+  # POST /resolutions
+  # POST /resolutions.xml
   def create
     @intend = Intend.new(:user_id => current_user.id, :resolution_id => params[:resolution_id])
 
@@ -10,6 +13,19 @@ class IntendsController < ApplicationController
         format.html { redirect_to(resolution_path(params[:resolution_id]), :notice => 'You already intend to do this.') }
         format.xml  { render :xml => @intend.errors, :status => :unprocessable_entity }
       end
+    end
+  end
+
+  # DELETE /intends/1
+  # DELETE /intends/1.xml
+  # 1 = resolution_id!
+  def destroy
+    @intend = current_user.intends.where(:resolution_id => params[:id])
+    @intend.destroy_all
+
+    respond_to do |format|
+      format.html { redirect_to(resolution_path(params[:id])) }
+      format.xml  { head :ok }
     end
   end
 end
